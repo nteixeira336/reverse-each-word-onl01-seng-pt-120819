fc8365cd44765f0c6b82bef0dabb466962746b7f
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-19 01:05:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
  reversed_sentence= [ ]
- sentence= "Hello there, and how are you?"
- sentence.split 
+sentence.split 
  sentence.map do |word|
    word.reverse 
  end 
