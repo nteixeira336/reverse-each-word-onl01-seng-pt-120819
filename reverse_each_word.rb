4c10cecd59aa9e391e268bd7dbcd43602b21fc9a
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-19 01:13:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
  split_sentence_array=sentence.split 
- split_sentence_array.map do |word|
+ reversed_array=split_sentence_array.map do |word|
    word.reverse 
  end 
 end 
